b0079ca55bada82cfb8cbaebef6a470ca5e30e7c
Jose Manuel <manuellh2008@gmail.com>
2019-06-28 02:36:17 -0500

    agregado vuetify y obtencion del listado

---
+++ src/components/GamesList.vue
@@ -0,0 +1,38 @@
+<template>
+<div>
+  <div
+    v-for="game in games"
+    :key="game.id"
+  >
+    {{ game.name }}
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import axios from 'axios';
+
+export default Vue.extend({
+  name: 'GamesList',
+  data() {
+    return {
+      games: {},
+    };
+  },
+  mounted() {
+    this.getGames();
+  },
+  methods: {
+    getGames() {
+      this.message = 'Hol';
+      axios.get('http://127.0.0.1:8000/api/juego')
+        .then((response) => {
+          this.games = response.data;
+        })
+        .catch(() => {
+        });
+    },
+  },
+});
+</script>
